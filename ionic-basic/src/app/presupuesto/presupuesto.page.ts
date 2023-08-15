535cd59223f76adfd13fe2d4d4f5787af829ec82
Cesar Saucedo <71368732+dlablo33@users.noreply.github.com>
2023-08-15 11:10:53 -0600

    Lunea_14_agosto
    Paso 1, falta de agregacion ultimos paso de clase

---
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presupuesto',
+  templateUrl: './presupuesto.page.html',
+  styleUrls: ['./presupuesto.page.scss'],
+})
+export class PresupuestoPage implements OnInit {
+  public
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ionic-basic/src/app/service/presupuesto.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Gasto } from '../interface/gasto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PresupuestoService {
+
+  constructor() { }
+}
